2a21071ecdd6bc44f80dbe4e2100230efb3a706c
Francesc Barragán Pascual <fbarraga@xtec.cat>
2023-02-01 10:05:54 +0100

    Update

--- Activitats/A6/app.py
+++ Activitats/A6/app.py
@@ -1,23 +1,23 @@
 import time
 import redis
-from flask import Flask 
+from flask import Flask
 
 
 app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379) 
+cache = redis.Redis(host='redis', port=6379)
 
 def get_hit_count():
-    retries = 5 
+    retries = 5
     while True:
         try:
             return cache.incr('hits')
-        except redis.exceptions.ConnectionError as exc: 
+        except redis.exceptions.ConnectionError as exc:
             if retries == 0:
-                raise exc 
-            retries -= 1 
+                raise exc
+            retries -= 1
             time.sleep(0.5)
 
-@app.route('/') 
+@app.route('/')
 
 def hello():
     count = get_hit_count()
